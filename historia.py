e5a4c6fb8a9ab7d6a663c1adbe0acd88989e2ab6
Julia Pontes <juliapontesilva06@gmail.com>
2024-10-26 11:38:52 -0300

    Add files via upload

---
+++ historia.py
@@ -0,0 +1,30 @@
+import random 
+
+# Função que gera a introdução da história 
+def gerar_introducao():
+    introducoes = ["Em uma noite chuvosa na metropole", "Num campo de trigo ao sul do texas"]
+    return random.choice(introducoes)
+
+# Função que gera o desenvolvimento da história 
+def gerar_desenvolvimento():
+    desenvolvimentos = ["a população clama por seu heroi", "um garoto muito especial é deixado na pora de um casal"]
+    return random.choice(desenvolvimentos)
+
+# Função que gera o final de toda a história
+def gerar_final():
+    finais = ["na esperança que o crime seja combatido.","afinal ele está destinado a salvar o mundo."]
+    return random.choice(finais)
+
+# Função principal que gera toda a história
+def gerar_historia():
+    introducao = gerar_introducao()
+    desenvolvimento = gerar_desenvolvimento()
+    final = gerar_final()
+
+    historia = f"{introducao} {desenvolvimento} {final}"
+    return historia
+
+# Exibe a história gerada 
+if __name__=="__main__":
+    print(gerar_historia())
+
